e6e65f5b8e7c3246470e847d64807346b870852d
edsan <jackwest415@gmail.com>
2020-04-13 00:57:56 +0300

    Design,v.1

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const amPm = document.querySelector(".amPm")
 const day = document.querySelector("#day")
 const name = document.querySelector("#name")
 const focus = document.querySelector("#focus")
+const focu = document.querySelector(".focus")
+const bg = document.querySelector(".container")
+const welcome = document.querySelector(".welcome")
+const lightGreen = "#f9fcfb"
 
 function showTime() {
   const date = new Date()
@@ -17,13 +21,13 @@ function showTime() {
   let displayDay = date.getUTCDay()
 
   let daysOfWeek = [
-    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
+    "Sunday"
   ]
 
   day.textContent = daysOfWeek[displayDay]
@@ -58,8 +62,92 @@ function setDisplay() {
   const date = new Date()
   //get time output
   let displayHour = date.getHours()
+
+  if (displayHour <= 12) {
+    bg.style.backgroundImage = 'url("./img/morning.jpg")'
+    welcome.textContent = "Good Morning"
+  } else if (displayHour <= 18) {
+    bg.style.backgroundImage = 'url("./img/afternoon.jpg")'
+    welcome.textContent = "Good Afternoon"
+    welcome.style.color = lightGreen
+    name.style.color = lightGreen
+    focus.style.color = lightGreen
+    focu.style.color = lightGreen
+  } else {
+    bg.style.backgroundImage = 'url("./img/evening.jpg")'
+    welcome.textContent = "Good Evening"
+    welcome.style.color = lightGreen
+    name.style.color = lightGreen
+    focus.style.color = lightGreen
+    focu.style.color = lightGreen
+  }
 }
+
+function getName() {
+  //check if the string-key in the has a obj or string
+  if (localStorage.getItem("name") === null) {
+    //if on keep on its init value
+    name.textContent = "{Enter Name}"
+  } else {
+    //if it has data get it
+    name.textContent = localStorage.getItem("name")
+  }
+}
+
+function setName(e) {
+  const vObj = e.target.innerText
+
+  console.log(vObj)
+  //check what type of event wat targeted
+  if (e.type === "keypress") {
+    //check of the key code
+    if (e.which == 13 || e.keyCode == 13) {
+      //setItems in local storage api with two params key "name", value "e.target.innerText"
+      localStorage.setItem("name", vObj)
+
+      //to blur the name
+      name.blur()
+    } else {
+      localStorage.setItem("name", vObj)
+    }
+  }
+}
+
+function getFocus() {
+  //check if the localstorage has item with the key to the value
+  if (localStorage.getItem("focus") === null) {
+    //return to its init value
+    focus.textContent = "{Enter focus}"
+  } else {
+    //get the data from the local storage
+    localStorage.getItem("focus")
+  }
+}
+
+function setFocus(e) {
+  //check what type of event wat targeted
+  if (e.type === "keypress") {
+    //check of the key code
+    if (e.which == 13 || e.keyCode == 13) {
+      //set items in the local str with two params
+      localStorage.setItem("focus", e.target.innerText)
+
+      //set on the choosen item
+      focus.blur()
+    } else {
+      localStorage.setItem("focus", e.target.innerText)
+    }
+  }
+}
+
+//add eventlistner
+name.addEventListener("keypress", setName)
+name.addEventListener("blur", setName)
+focus.addEventListener("keypress", setFocus)
+focus.addEventListener("blur", setFocus)
 
 //run functions
 showTime()
 setDisplay()
+getFocus()
+getName()
